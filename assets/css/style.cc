92a59afa0caea75e572827f5ed0cb5dbd16ba447
Kiswandi Chow <44989826+kiswandichow@users.noreply.github.com>
2019-02-22 11:22:32 +0700

    create style.css

---
+++ assets/css/style.cc
@@ -0,0 +1,74 @@
+
+.container{
+	background: #0ba889;
+	padding:15px 0 0 0;
+	margin:0;
+	width: 280px;
+	border: 1px solid #d4d4d4;
+	
+}
+
+table {
+	border : none;
+	border-collapse: collapse;
+}
+
+td{
+	padding: 0;
+	border: none;
+	background: white;
+}
+
+input[type=text]{
+	padding: 10px;
+	width: 258px;
+	text-align: right;
+	font-size: 30px;
+	border: 1px solid #ffffff;
+	height: 50px;
+}
+
+input[type=text]:disabled{
+	background-color: white;
+}
+
+.btn {
+	width: 70px;
+	height: 50px;
+	background-color: #d4d4d4;
+    border: none;
+    color: #000000;
+    padding: 10px;
+	font-size: 16px;
+	cursor: pointer;
+}
+
+.btn:hover {
+	background-color: #00725b;
+	color: #ffffff;
+}
+
+
+.opr{
+	background-color: #bcbcbc;
+	color: #0ba889;
+	font-weight: bold;
+	font-size: 20px;
+	border-bottom: 1px solid #ffffff; 
+}
+
+.opr2{
+	background-color:#6a6a6a;
+	color: #ffffff;
+	font-weight: bold;
+	font-size: 20px;
+}
+
+.mid{
+	border-right: 1px solid #ffffff; 
+	border-bottom: 1px solid #ffffff; 
+}
+
+.bottom{
+	border-right: 1px solid #ffffff;
+}
